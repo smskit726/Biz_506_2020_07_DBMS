6882e3bdb0ee6b06b1e69f23eaecf17c6c3b87f8
smskit726 <smskit726@gmail.com>
2020-07-14 16:42:41 +0900

    sql_CRUD

---
+++ 관리자(2020-07-14).sql
@@ -0,0 +1,128 @@
+-- Comment : 한줄 주석
+/*
+    여러줄 주석
+    SQL 명령을 입력할 때 명령이 끝났다라는 것을 알리기 위해 ; 붙여준다.
+    
+    Ctrl + Enter : 현재 커서가 있는 곳의 명령문을 DBMS로 보내고 결과를 받기
+*/
+
+/*
+현재 오라클에 접속된 사용자(sys)가 관리하는 table들이 있는데 그 중에서 tab이라는
+이름의 tabale정보를 가져와서 나에게 보여달라!
+
+오라클의 tab table은 현재 사용자가 관리하는 DB Object(객체)들의 정보를
+보관하고 있는 table
+*/
+SELECT *
+FROM tab;
+
+
+-- ALL_ALL_TABLES : 오라클 system 데이터 사전의 자세한 정보를 보관하는 table
+SELECT *
+FROM ALL_ALL_TABLES;
+
+
+/*
+SELECT 키워드는 FROM 절을 포함하는 명령문 형태로 작성을 하며 
+DBMS가 보관하고 있는 데이터를 table 형식으로 보여달라 라는 명령
+DBMS의 DML(Database Manuplation Language)중에서 Read(조회)를 수행하는 명령
+CRUD 중에서 R : Read, Retrive 를 수행하는 명령문
+*/
+SELECT *
+FROM 주소록;
+
+
+/*
+SQL 명령문을 통해서 DB객체를 만들고 삭제하고, 데이터를 추가, 변경, 삭제를 수행할텐데
+sys 사용자로 접속을 하게 되면 중요한 정보를 잘못 삭제, 변경할 우려가 있기 때문에
+실습을 위해서 사용자를 생성하여 수행을 할 것이다.
+
+사용자를 추가하는 순서
+1. Table Space : 데이터를 저장할 물리적 공간을 설정
+2. User : 사용자를 생성하고, 물리적 저장공간과 연결
+*/
+
+
+/*
+- TableSpace 생성(Create)
+TableSpace는 오라클에서 Data를 저장할 물리적 공간을 설정하는 것
+
+myTS : 앞으로 SQL을 통해서 사용할 TableSpace의 Alias(이름)
+'.../myTS.dbf' : 저장할 파일 이름
+
+SIZE : 오라클에서는 성능의 효율성을 주기위해 일단 빈 공간을 일정부분 설정하게 된다.
+    크기는 최초에 저장할 데이터의 크기등을 계산하여 설계하고 설정한다.
+    너무 작으면 효율성이 떨어지고, 너무 크면 불필요한 공간을 낭비한다.
+    오라클 xe(express Edition)에서는 table Space의 최대 크개를 11G로 제한한다.
+    만약 Size를 10G로 지정하고, 용량이 초과되어 AUTO NEXT로 추가가 되는 경우
+    전체 Size가 11G를 넘어서면 오류가 나면서 더이상 데이터를 저장할 수 없게 된다.
+    
+AUTO...NEXT : 만약 초기에 지정한 SIZE공간에 데이터가 가득차면 자동으로 용량을 늘려서
+    저장할수 있도록 만들어라
+    
+SIZE의 1M : 기본크기를 1024*1024 byte 크기로 지정하라.
+            SIZE를 지정할 때 1MB라고 하지 않는다.
+NEXT 500K : 자동으로 확장(늘리기)를 1024*500 크기로 설정
+            NEXT를 지정할 때 500KB라고 하지 안는다.
+            
+CREATE로 시작되는 명령문 : DDL(Data Definition Language) : 데이터 선언, 생성(추가와는 다름)
+*/
+CREATE TABLESPACE myTS
+DATAFILE 'C:/bizwork/workspace/oracle_data/myTS.dbf'
+SIZE 1M AUTOEXTEND ON NEXT 500K;
+
+/*
+- 질의작성기에서 코드를 작성할 때 약속
+DBMS의 SQL문은 특별한 일부 경우를 제외하고 대소문자 구별을 하지 않는다.
+DBMS, SQL, 오라클과 관련된 키워드는 모두 대문자로 작성할 것
+변수, 값, 내용 등은 소문자로 사용하며 특별히 대소문자를 구분해야 하는 경우는
+    별도 공지를 할 것
+    
+DROP : DDL명령의 CREATE와 반대되는 개념의 명령문
+DROP 명령은 데이터를 물리적으로 완전삭제하는 개념이므로 매우 신중하게 사용해야한다.
+*/
+DROP TABLESPACE myTS  -- myTS tableSpace를 삭제하면서
+INCLUDING CONTENTS AND DATAFILES  -- 연관된 정보와 data file도 같이 삭제하고
+CASCADE CONSTRAINTS;  -- 그리고 설정된 권한, 역할 등이 있으면 그들도 같이 삭제하라
+
+
+-- 위에서 생성한 TableSpace를 관리하며, 데이터를 조작할 사용자를 생성
+CREATE USER user1 IDENTIFIED BY 1234  -- 사용자 ID를 user1으로 설정하고 초기 비밀번호를 1234로 설정
+DEFAULT TABLESPACE myTS;
+
+
+-- DCL(Data Control Language)
+-- 새로 생성된 user1에게 권한을 부여하자
+GRANT CONNECT TO user1;  -- user1이 로그인만 할 수 있도록 권한(역할)을 부여
+REVOKE CONNECT FROM user1;  -- user1이 로그인 할 수 있는 권한을 제거
+
+
+-- user1이 로그인을 수행하고, 최소한으로 데이터들을 관리할 수 있도록 권한을 부여
+/*
+RESOURCE : 오라클에서 USER에게 줄 수 있는 권한 중 상당히 많은 일을 수행할 수 있는 권한
+        현재 시스템에 설치된 모든 TableSpace를 대상으로 무제한(TableSpace가 허용하는범위)
+        저장이 가능한 권한
+        RESOURCE 권한은 Standard, Enterprise DBMS에서는 함부로 부여해서는 안된다!
+        
+        CONNECT 와 RESOURCE 권한을 부여하게 되면 거의 DBA 수준의 권한을 갖게 된다.     
+*/
+GRANT CONNECT,RESOURCE TO user1;
+
+-- login 권한과 table을 생성할 수 있는 권한
+GRANT CONNECT,CREATE TABLE TO user1;
+-- login 권한과 table 생성권한, 학생정보 테이블에 데이터를 추가할 수 있는 권한
+GRANT CONNECT,CREATE TABLE,INSERT TABLE 학생정보 to user1;
+-- login, talbe 생성, 학생정보 추가, 학생정보 조회할 수 있는 권한
+GRANT CONNECT,CREATE TABLE,INSERT TABLE 학생정보,SELECT TABLE 학생정보 to user1;
+
+/*
+권한을 세부적으로 부여하는 것은 실무상에서 매우 필요하며 중요한 일이다.
+하지만, 학습하는 입장에서 GRANT 부여하는데 너무 많은 노력을 보이면 불편하니 xe 버전에서는
+사용자에게 DBA권한을 부여하고, 실습을 수행한다.
+*/
+GRANT DBA TO user1;
+
+/*
+DBA권한(Roll)은 sysDBA보다 한단계 낮은 권한 등급을 가지며,
+일반적으로 자신이 생성한 Table 등 DB Object에만 접근하여 명령을 수행한다.
+*/
--- 오라클 DBMS.md
+++ 오라클 DBMS.md
@@ -1,4 +1,4 @@
-오라클 DBMS
+# 오라클 DBMS
 1. 표준 SQL을 사용하는 데이터베이스 소프트웨어
 2. 대용량, 분산, 고신뢰성을 보여주는 대표적인 DBMS SW
 3. DBMS SW 스키마 : 저장공간 단위, 그룹
@@ -8,4 +8,37 @@
 7. 사용자를 등록하면 사용자 자체가 Schema가 되며, 사용자별로 데이터를 저장하는 물리적 공간을 OS에서 관리하는 파일 단위 개념으로 사용한다.
 8. Table Space : DB를 저장하는 논리적인 개념보다는 실제 데이터가 저장되는 파일이라고 보면 된다.
 9. 사용자를 등록하기 전에 Table Space를 만들고, 사용자를 등록하면서 생성한 Table Space에 데이터를 저장하도록 연결하는 작업이 필요하다.
+10. 오라클 DBMS로 실습을 하면서 혹시나 물리적인 DB가 저장되는 폴더나, Table Space와 연결된 파일을 git 올리지 않아야 한다.
+11. 오라클 DBMS Table Space와 연결된 파일은 용량이 상대적으로 매우 커서 git에 올리게 되면 문제가 발생할 수 있다.
 
+### sys 사용자 접속 설정
+1. 오라클 DBMS SW를 설치하면 기본적으로 DB Administer 사용자가 등록이 되어 있다.
+2. System DBA : 오라클 DBMS을 관리하는 최상위 등급의 사용자
+3. ID가 sys user : SQL Developer를 통해서 오라클 DBMS에 접속하여 Schema, User, Table등을 생성하거나 삭제 할 수 있는 권한을 가진 사용자
+4. sys user는 오라클 DBMS를 설치할 때 설정한 비밀번호로 접속을 할 수 있다.
+
+### 오라클 사용자 생성
+1. sys로 접속하여 SQL 실습, 개발, 운용을 하는 것은 보안상, 안전상 매우 좋지 않다.
+2. 일반적으로 사용자를 등록하고, 그 사용자 ID로 접속하여 명령을 수행하는 것이 좋다.
+
+### 오라클의 사용자 생성 절차
+1. 사용자가 사용(관리)할 TableSpace를 생성한다.
+2. User를 생성하면서 TableSpace를 default로 지정해준다.
+3. 오라클에서는 새로운 사용자를 등록하면 아무것도 할수 없는 상태로 ID만 생성을 한다.
+4. 새로운 사용자가 DB에 접속하여 뭔가 업무를 수행하려면, 관리자(sys)가 새로운 사용자에게 권한을 부여해 주어야 한다.
+
+
+### NULL 값
+* 프로그램 코딩이나, DBMS에서 사용된 NULL 값
+* 문자열형 : 공백, 빈칸 처럼 보이지는 않지만 Code값으로 인식되는 데이터와 구분하기 위해서 사용하는 '아무것도 아닌 값' 이라는 개념
+* String s = " ", String s = "", String s = null
+* DB에서 Null 값은 칼럼에 데이터가 추가되지 않은 상태 
+* INSERT 를 수행하면서 해당 칼럼의 데이터를 지정하지 않았을 때
+
+* DBMS PK로 지정된 칼럼이나, NOT NULL로 지정된 칼럼은 데이터를 지정하지 않은 상태로 INSERT명령을 수행할 수 없다.
+
+## DataBase Language
+1. DB Lang. 에는 DDL, DML, DCL 명령 세트가 있다.
+2. DDL(Data Definition Lang.) : CREATE, DROP, ALTER
+3. DML(Data Maniplation Lang.) : INSERT, SELECT, UPDATE, DELETE
+4. DCL(Data Control Lang.) : GRANT, REVOKE, COMMIT, ROLLBACK
