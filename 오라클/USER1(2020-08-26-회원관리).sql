716f1c8e883aab8e177492f3601eac1b12442526
smskit726 <smskit726@gmail.com>
2020-08-26 16:41:26 +0900

    tbl_member

---
+++ 오라클/USER1(2020-08-26-회원관리).sql
@@ -0,0 +1,11 @@
+-- 여기는 USER1 화면입니다.
+CREATE TABLE tbl_member (
+        M_USERID	VARCHAR2(50)		PRIMARY KEY,
+        M_PASSWORD	VARCHAR2(125)	NOT NULL,	
+        M_NAME	nVARCHAR2(30)	NOT NULL,	
+        M_TEL	VARCHAR2(20),		
+        M_ADDRESS	nVARCHAR2(255),		
+        M_ROLE	VARCHAR2(20)		
+);
+
+SELECT * FROM tbl_member;
